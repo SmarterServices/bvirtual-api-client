8ddf542c12676982bd60ca7aa9a92c1e3421e455
Ashik Ahmed <ashik.ahmed@enosisbd.com>
2017-02-26 16:22:23 +0600

    #140191485 - Create a test method with unit tests

--- test/lib/client.js
+++ test/lib/client.js
@@ -1,7 +1,29 @@
 'use strict';
+const expect = require('chai').expect;
+
+const mockRequest = require('./mock');
+
+const BVirtualClient = require('./../../index');
+
+const correctToken = 'correctToken',
+  wrongToken = 'wrongToken',
+  url = 'http://localhost:5678';
+
+//Create mock response for the requests
+mockRequest(url);
 
 describe('Testing B Virtual API client', function () {
-  it('Test', function (done){
-    done();
+  describe('Testing testRequest', function () {
+    it('Should take correct token and resolve with working as true', function () {
+
+      let client = new BVirtualClient({url, token: correctToken});
+
+      return client
+        .testRequest()
+        .then(function (data) {
+          return expect(data.working).to.be.equal(true);
+        })
+    });
   });
+
 });
--- test/lib/mock.js
+++ test/lib/mock.js
@@ -1,6 +1,28 @@
 'use strict';
+const nock = require('nock');
 
-const mockRequests = {
+const correctTokenHeader = {
+    reqheaders: {
+      authorization: 'BASIC correctToken'
+    }
+  },
+  wrongTokenHeader = {
+    reqheaders: {
+      authorization: 'BASIC wrongToken'
+    }
+  };
+
+const mockRequests = function mock(url) {
+
+  nock(url, correctTokenHeader)
+    .get('/test')
+    .reply(200, {working: true});
+
+  nock(url, wrongTokenHeader)
+    .get('/test')
+    .reply(307, {working: false, message: 'Incorrect password !'});
+
+
 };
 
 module.exports = mockRequests;
