7dfdceb796fd6f5bdcebcfc48bbd90196c05cf70
Ashik Ahmed <ashik.ahmed@enosisbd.com>
2017-02-26 13:08:59 +0600

    #140191485 - Create project structure

---
+++ test/lib/client.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Testing B Virtual API client', function () {
+  it('Test', function (done){
+    done();
+  });
+});
---
+++ test/lib/mock.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const mockRequests = {
+};
+
+module.exports = mockRequests;
+
